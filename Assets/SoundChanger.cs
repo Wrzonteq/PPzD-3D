fee62347515e88883ff5d6ef85f2398187391553
Bartłomiej Łakuta <47048050+Telioner@users.noreply.github.com>
2020-11-29 10:36:27 +0100

    Laboratory

---
+++ Assets/SoundChanger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundChanger : MonoBehaviour
+{
+
+	public AudioSource ambientPlayer;
+	public AudioClip outsideAmbient;
+	public AudioClip insideAmbient;
+
+	public int triggerCounter = 0;
+	private void OnTriggerEnter(Collider other)
+	{
+		triggerCounter++;
+		if (other.tag == "Inside" && triggerCounter == 1)
+		{
+			ambientPlayer.clip = insideAmbient;
+			ambientPlayer.Play();
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		triggerCounter--;
+		if (other.tag == "Inside" && triggerCounter == 0)
+		{
+			ambientPlayer.clip = outsideAmbient;
+			ambientPlayer.Play();
+		}
+	}
+}
