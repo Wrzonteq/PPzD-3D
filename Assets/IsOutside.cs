6141118e3d99f7ae76fb1ee5daa1a051d7fe48a5
Jappowski <58007440+Jappowski@users.noreply.github.com>
2020-12-04 14:01:41 +0100

    Zadanie z zajęć nr 5

---
+++ Assets/IsOutside.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsOutside : MonoBehaviour
+{
+ 
+    void OnTriggerEnter(Collider other)
+    {
+        GameObject.Find("inTheBuildCheck").GetComponent<inTheBuild>().inSide = false;
+    }
+}
---
+++ Assets/inTheBuild.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+   
+public class inTheBuild : MonoBehaviour
+{
+   public void Update()
+   {
+      setSfxLvl();
+   }
+   public bool inSide = false;
+   void OnTriggerEnter(Collider other)
+   {
+      if (inSide == false)
+         inSide = true;
+      else
+      {
+         inSide = false;
+      }
+      
+   }
+
+   [SerializeField] private AudioMixer sfx;
+    
+   void setSfxLvl()
+   {
+      if (inSide)
+      {
+         sfx.SetFloat("windSound" , -20f);
+      }
+      else
+      {
+         sfx.SetFloat("windSound", 0f);
+      }
+   }
+ 
+
+}
