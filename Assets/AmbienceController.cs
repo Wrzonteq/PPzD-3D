e595c0f2b9bc25cf9ed62e4cf9f5d90f88a7ddf6
Buovsky <57236090+Buovsky@users.noreply.github.com>
2020-12-11 19:46:39 +0100

    PPzD_Lab5_Jakub_Starzec_11822

---
+++ Assets/AmbienceController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AmbienceController : MonoBehaviour
+{
+    [SerializeField] private AudioMixer masterAudioMixer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider trigger)
+    {
+        if(trigger.gameObject.tag == "Player")
+        {
+            masterAudioMixer.FindSnapshot("InLab").TransitionTo(1.5f);
+        }
+    }
+    private void OnTriggerExit(Collider trigger)
+    {
+        if (trigger.gameObject.tag == "Player")
+        {
+            masterAudioMixer.FindSnapshot("OutLab").TransitionTo(1.5f);
+        }
+    }
+}
