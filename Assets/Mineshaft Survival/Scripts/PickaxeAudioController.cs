27ab52ef713fcf28d7329fc2b4528becc083d941
Kamil Jamróz <nergydev@gmail.com>
2020-12-13 08:32:15 +0100

    copy from old fork

---
+++ Assets/Mineshaft Survival/Scripts/PickaxeAudioController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PickaxeAudioController : MonoBehaviour {
+    [SerializeField] ObjectUsage objectUsage;
+    [SerializeField] AudioSource inventory;
+    [SerializeField] AudioClip pickaxeHitAudioSource;
+    [SerializeField] AudioClip pickaxeWallHitAudioSource;
+    private void Start() {
+        inventory.bypassReverbZones = true;
+        objectUsage.pickaxeHit.AddListener(() => inventory.PlayOneShot(pickaxeHitAudioSource));
+        objectUsage.pickaxeWallHit.AddListener(() => inventory.PlayOneShot(pickaxeWallHitAudioSource));
+    }
+}
---
+++ Assets/_Code/CaveGeneratorAudioController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class CaveGeneratorAudioController : MonoBehaviour {
+    [SerializeField] AudioSource generatorAudioSource;
+    [SerializeField] Generator generator;
+
+    void Update() {
+        generatorAudioSource.volume = generator.Toggled ? 1f : 0f;
+    }
+}
