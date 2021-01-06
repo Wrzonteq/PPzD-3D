1ced885197681229b2d93b244d77cc4febf0b90e
Wiktor Dukacz <viktordukacz@onet.pl>
2021-01-06 21:44:48 +0100

    lab5_outpost_on_desert

---
+++ Assets/_Code/InsideOutsideCheck.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class InsideOutsideCheck : MonoBehaviour
+{
+    [SerializeField] private AudioMixerSnapshot insideSound;
+    [SerializeField] private AudioMixerSnapshot outsideSound;
+    private const float transitionDuration = .5f;
+    
+    void OnTriggerEnter(Collider other) {
+        insideSound.TransitionTo(transitionDuration);
+    }
+
+    void OnTriggerExit(Collider other) {
+        outsideSound.TransitionTo(transitionDuration);
+    }
+}
