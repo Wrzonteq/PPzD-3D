6ef325032b89fefc9db70e8d22c5a418fd08352a
Patryk Szczesiak <p.szczesiak984@gmail.com>
2021-02-03 19:13:21 +0100

    Lab5

---
+++ Assets/_Code/Ambience_Change.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Ambience_Change : MonoBehaviour
+{
+    [SerializeField] AudioMixerSnapshot outside;
+    [SerializeField] AudioMixerSnapshot inside;
+    [SerializeField] bool insides = false;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && insides)
+        {
+            inside.TransitionTo(0.5f);
+        }
+        if (other.gameObject.CompareTag("Player") && !insides)
+        {
+            outside.TransitionTo(0.5f);
+        }
+    }
+}
